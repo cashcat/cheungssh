ec6bb0fd3de00b6f7d18fe39edd5553867d5d121
root <root@CheungSSH.(none)>
2017-04-05 14:30:22 +0800

    update

--- mod_python-3.4.1/test/htdocs/dummymodule.py
+++
@@ -1,7 +0,0 @@
-from mod_python import apache
-
-apache.log_error("dummymodule / %s" % apache.interpreter)
-
-def function():
-    apache.log_error("dummymodule::function / %s" % apache.interpreter)
-    apache.main_server.get_options()["dummymodule::function"] = "1"
